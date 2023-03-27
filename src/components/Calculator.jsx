9d78c0e6b7221a8197d7a299a37d5ed5eb204dac
danutibe07 <danielutibe07@gmail.com>
2023-03-27 12:46:48 +0100

    correct linter errors

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,34 +1,33 @@
 import React from 'react';
 
-const Calculator = () =>{
-return(
+const Calculator = () => (
   <div>
     <div className="calculator">
       <div className="screen">
         0
-     </div>
+      </div>
       <div className="calculator-buttons">
-        <button   type="button">AC</button>
-        <button   type="button">+/-</button>
-        <button   type="button">%</button>
-        <button   type="button" className="orange-btn">÷</button>
-        <button   type="button">7</button>
-        <button   type="button">8</button>
-        <button   type="button">9</button>
-        <button   type="button" className="orange-btn">x</button>
-        <button   type="button">4</button>
-        <button   type="button">5</button>
-        <button   type="button">6</button>
-        <button   type="button" className="orange-btn">-</button>
-        <button   type="button">1</button>
-        <button   type="button">2</button>
-        <button   type="button">3</button>
-        <button   type="button" className="orange-btn">+</button>
-        <button   type="button" className="span2">0</button>
-        <button   type="button">.</button>
-        <button   type="button" className="orange-btn">=</button>
+        <button type="button">AC</button>
+        <button type="button">+/-</button>
+        <button type="button">%</button>
+        <button type="button" className="orange-btn">÷</button>
+        <button type="button">7</button>
+        <button type="button">8</button>
+        <button type="button">9</button>
+        <button type="button" className="orange-btn">x</button>
+        <button type="button">4</button>
+        <button type="button">5</button>
+        <button type="button">6</button>
+        <button type="button" className="orange-btn">-</button>
+        <button type="button">1</button>
+        <button type="button">2</button>
+        <button type="button">3</button>
+        <button type="button" className="orange-btn">+</button>
+        <button type="button" className="span2">0</button>
+        <button type="button">.</button>
+        <button type="button" className="orange-btn">=</button>
       </div>
     </div>
   </div>
-)};
+);
 export default Calculator;
