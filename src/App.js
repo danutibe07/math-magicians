4e6aefaa5a6a97de2a814085abf0bff3c3f73145
danutibe07 <danielutibe07@gmail.com>
2023-04-03 18:15:21 +0100

    updates to navbar

--- src/App.js
+++ src/App.js
@@ -3,18 +3,16 @@ import Calculator from './components/Calculator';
 import Header from './components/Header';
 import Quotes from './components/Quotes';
 import Home from './components/Home';
-import Error from './components/Error';
 
 function App() {
   return (
     <div className="math-magician">
-      <Router>
+      <Router basename="math-magicians">
         <Header />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/calculator" element={<Calculator />} />
           <Route exact path="/quotes" element={<Quotes />} />
-          <Route path="/quotes" element={<Error />} />
         </Routes>
       </Router>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => (
   <nav className="maths-navbar">
-    <NavLink to="/home">Home</NavLink>
+    <NavLink to="/">Home</NavLink>
     <NavLink to="/calculator">Calculator</NavLink>
     <NavLink to="/quotes">Quote</NavLink>
   </nav>
